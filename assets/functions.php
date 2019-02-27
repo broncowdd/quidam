a04a9e6ac9ad8544c82d775453d146301eb3b488
Bronco <broncowdd@users.noreply.github.com>
2019-02-27 07:49:54 +0100

    added file_curl_contents function

--- assets/functions.php
+++ assets/functions.php
@@ -33,3 +33,21 @@ function deleteDir($dir){
         rmdir($dir); 
     }       
 }
+function file_curl_contents($url){
+
+    if (function_exists('curl_init')){
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER,  FALSE);     
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);     
+        curl_setopt($ch, CURLOPT_URL, $url);     
+        if (!ini_get("safe_mode") && !ini_get('open_basedir') ) {curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);}    
+        curl_setopt($ch, CURLOPT_MAXREDIRS, 10); 
+        curl_setopt($ch, CURLOPT_REFERER, 'http://oudanstoncul.free.fr');// notez le referer "custom"
+        $data = curl_exec($ch);     
+        curl_close($ch);     
+    }else{
+        $data=file_get_contents($url);
+    }
+    return $data; 
+}
